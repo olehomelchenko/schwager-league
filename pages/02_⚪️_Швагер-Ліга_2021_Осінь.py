511640b8fb57616dc3941f80b08ac0a3582d0437
Oleh Omelchenko <ptrvtch+github@protonmail.com>
2022-09-18 19:27:01 +0300

    rename files

---
+++ pages/02_⚪️_Швагер-Ліга_2021_Осінь.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+from utils import (
+    read_stats_to_dataframe,
+    create_tabs,
+)
+
+st.set_page_config(layout="wide")
+st.markdown("# Швагер-Ліга 2021 - Осінь")
+st.markdown(
+    "Вихідні дані взяті з гугл-таблиці за [посиланням](https://docs.google.com/spreadsheets/d/1s17oVRKJ1px4EWxQ3Fl1pbxOVSMYNKZYnD8oRQFbaNY/edit#gid=0)"
+)
+st.markdown("Помітили помилку? Пишіть [Олегу](https://fb.com/ptrvtch)")
+
+
+def main():
+    st.markdown("*** to be added***")
+
+
+main()
